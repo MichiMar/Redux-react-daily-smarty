29ff5ecc6649c74ab6dd4e32d9df9825726c278a
MichiMar <michimar.alexis@gmail.com>
2022-12-26 21:59:48 -0600

    Added titles for each post

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,7 @@
 import {
     SET_RECENT_POSTS
 } from '../actions/types';
-import recentPosts from '../components/recentPosts';
+// import recentPosts from '../components/recentPosts';
 
 const INIT_STATE = {
     resultsPosts: [],
@@ -11,7 +11,11 @@ const INIT_STATE = {
 export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
-            return [...state, recentPosts: action.payload]
+            const recentPosts = action.payload;
+            return {
+                ...state,
+                recentPosts
+            }
             default:
                 return state
     }
