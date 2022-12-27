9a34a295646ad0e1caeb7f8d33a7cc97e7b4a7f7
MichiMar <michimar.alexis@gmail.com>
2022-12-27 00:21:04 -0600

    Results posts created, API is not working

--- src/components/results.js
+++ src/components/results.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+
 import Logo from "./logo";
 import SearchBar from "./searchBar";
+import ResultsPosts from "./resultsPosts";
+
 
 import { connect } from "react-redux";
 import * as actions from '../actions';
@@ -15,6 +18,7 @@ class Results extends Component {
             <div>
                 <Logo size={105}/>
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <ResultsPosts/>
             </div>
         )
     }
